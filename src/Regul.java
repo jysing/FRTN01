ec75d8116746cfcad9dcb04526efddc8a8b688d8
Raddle <ael10rpe@student.lu.se>
2014-11-20 09:08:31 +0100

    Gyro klassen

--- src/Regul.java
+++ src/Regul.java
@@ -1,4 +1,17 @@
-
+import lejos.hardware.sensor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.Device.*;
+import lejos.hardware.ev3.*;
+import lejos.robotics.SampleProvider;
 public class Regul extends Thread {
+			
+			public Regul(){
+				
+			}
+			
 
+			
+			
 }
+
+
