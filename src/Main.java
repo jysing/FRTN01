1fdc5b077c7178ee36f53a8206b44291e991b499
Raddle <ael10rpe@student.lu.se>
2014-11-20 09:22:10 +0100

    Gyro klassen2

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,13 @@ public class Main {
 	Communication m = new Communication();
 	
 	Gyro g = new Gyro();
-	g.getAngleVelocity();
+	float[] data = g.getAngleVelocity();
+	LCD.drawString("AngleVelocity : "+data[0],2,0);
+	Delay.msDelay(3000);
+	LCD.clear();
+	LCD.drawString("AngleVelocity2 : "+data[0],2,0);
+	Delay.msDelay(3000);
+	
 	
 }
 }
