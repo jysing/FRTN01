20a22e0a39a916f35e0e68cf63b4a2077e0f02ec
Raddle <ael10rpe@student.lu.se>
2014-11-21 09:40:39 +0100

    getAnglevel

--- src/Gyro.java
+++ src/Gyro.java
@@ -8,52 +8,52 @@ import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 
-public class Gyro {
+public class Gyro extends Thread{
 	private Port port;
 	private HiTechnicGyro sensor;
 	public SampleProvider rate;
 	public float sample[];
+	public float offset=0;
 	
 	
+	//Gyro can deliver 300 measurements per sample
 	public Gyro(){
 		
 		port = LocalEV3.get().getPort("S1");
 		sensor = new HiTechnicGyro(port);
 		
 		sample = new float[sensor.sampleSize()];
-		int offset  = calculateOffset();
+		calculateOffset();
 		while (!Button.ESCAPE.isDown()) {
-	         sensor.fetchSample(sample, offset);
-	         LCD.drawString(String.format("%3.2f", sample[0]) + " m        ", 0, 3);
+	         sensor.fetchSample(sample,0);
+	         LCD.drawString(String.format("%3.2f", sample[0]-offset) + " m        "+ sensor.sampleSize(), 0, 3);
 	      }
 		sensor.close();
 		
 		
 		
 	}
-	public float[] getAngleVelocity(){
-			rate.fetchSample(sample, 0);
-			return sample;
+	public float getAngleVelocity(){
+			sensor.fetchSample(sample, 0);
+			return sample[0]-offset;
 		
 	}
 	//Beräkna offset i gyrosensor
-	public int calculateOffset() {
+	private void calculateOffset() {
 		int count = 10;
-		float offset=0;
 		for(int i = 0; i<count; i++){
 			sensor.fetchSample(sample,0);
 			offset = offset + sample[0];
 			try {
-				wait(10);
+				sleep(10);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		offset = offset/count;
-		offset = Math.round(offset);
+		//offset = Math.round(offset);
 		
-		return (int)offset;
 	}
 }
 
