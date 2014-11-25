25349febdb39eb944220356c419cae958b9de895
jysing <joakim.ysing@gmail.com>
2014-11-25 09:14:26 +0100

    started threads

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 	public static void main(String [] args){
 		Communication m = new Communication();
 		Gyro gyro = new Gyro(1);
+		gyro.start();
 		Regul regul = new Regul(gyro,2); //Next highest priority.
 									//Reason is to get more accurate samples.
 									//Might need to be changed to highest later on.
+		regul.start();
 	}
 }
