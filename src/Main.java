92cc75729130bf36908f1f93711a36ee5d5d9192
jysing <joakim.ysing@gmail.com>
2014-11-25 11:26:55 +0100

    new program

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import lejos.utility.Delay;
 public class Main {
 	
 	public static void main(String [] args){
-		Gyro gyro = new Gyro(1);
-		gyro.start();
+		Gyro gyro = new Gyro();
+		//gyro.start();
 		Regul regul = new Regul(gyro,2); //Next highest priority.
 									//Reason is to get more accurate samples.
 									//Might need to be changed to highest later on.
