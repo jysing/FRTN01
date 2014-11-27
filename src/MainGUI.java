0135d344b215e374539e1b302e13fa11d4cd2f21
Fatmajk <michael.kapusta@live.se>
2014-11-27 10:34:46 +0100

    mainGUI

---
+++ src/MainGUI.java
@@ -0,0 +1,13 @@
+public class MainGUI {
+	SocketClient 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+		Graph graph = new Graph(comm);
+		} catch (Exception e){
+			System.out.println("Cannot create graph()");
+		}
+	}
+
+}
