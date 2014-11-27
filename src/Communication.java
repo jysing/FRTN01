0b7f8f04d50ad0c294707f4658d3b1acadc61c26
jysing <joakim.ysing@gmail.com>
2014-11-27 08:46:01 +0100

    new tcp attempt

--- src/Communication.java
+++ src/Communication.java
@@ -3,75 +3,99 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import lejos.hardware.Bluetooth;
-import lejos.hardware.lcd.LCD;
-import lejos.remote.ev3.RemoteEV3;
-import lejos.remote.nxt.NXTCommConnector;
-import lejos.remote.nxt.NXTConnection;
+//package uk.co.moonsit.sockets;
+/**
+ *
+ * @author Rupert Young
+ */
+public class Communication {
 
-public class Communication extends Thread {
-	
-	RemoteEV3 ev3;
+	private final ServerSocket serverSocket;
+	private Socket server;
+	private DataOutputStream out;
+	private DataInputStream in;
 
-	public Communication() {
-		this.start();
+	public Communication(int port, int timeout) throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(timeout);
 	}
 
-	public void run(){
-		Boolean isrunning=true;
-		
-		while (true)
-		{			
-			LCD.drawString("Waiting",0,0);
-			//Listen for incoming connection
-			try{
-				ev3 = new RemoteEV3("130.235.126.71");
-				ev3.getBluetoothDevice().authenticate("130.235.126.71", "123456");
-				LCD.drawString("Success.", 0, 0);
-				break;
-				} catch (Exception e){
-					
-			}
-			/*
-			NXTCommConnector btc = Bluetooth.getNXTCommConnector();
-			LCD.drawString("Got connector",0,1);
-			NXTConnection connection = btc.waitForConnection(10000,NXTConnection.RAW);
-			LCD.drawString("Connected",0,1);
-			//The InputStream for read data 
-			DataInputStream dis = connection.openDataInputStream();
-			//loop for read data    
-			while(isrunning){
-				Byte n=-1;
-				try {
-					n=dis.readByte();
-				} catch (IOException e) {
-					LCD.drawString("Reading from input aborted", 0, 0);
-				}
-				LCD.clear();
-				LCD.drawInt(n, 4, 4);
-			}
+	public void connect() throws IOException {
+		System.out.println("Wait on " + serverSocket.getLocalPort() + "...");
+		server = serverSocket.accept();
+
+		System.out.println("Connected to " + server.getRemoteSocketAddress());
+		out = new DataOutputStream(server.getOutputStream());
+		in = new DataInputStream(server.getInputStream());
+	}
+
+	public int getPort() {
+		int port = 0;
+		if (serverSocket != null) {
+			port = serverSocket.getLocalPort();
+		}
+		return port;
+	}
+
+	public boolean isConnected() {
+		if (server == null) {
+			return false;
+		}
+		return server.isConnected();
+	}
+
+	public String socketStatus() {
+		return server == null ? "null" : "not null";
+	}
+
+	public void connectionLost() {
+		server = null;
+	}
+
+	public String sendAndReceive(String message) throws IOException {
+		out.writeUTF(message);
+		String response = in.readUTF();
+		return response;
+	}
+
+	@SuppressWarnings("null")
+	public void theMain() {
+		int port = 6666;
+		int timeout = 30000;
+		Communication ss = null;
+
+		try {
+			ss = new Communication(port, timeout);
+		} catch (IOException ex) {
+			Logger.getLogger(Communication.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+
+		try {
+			ss.connect();
+		} catch (java.net.SocketTimeoutException ex) {
+			Logger.getLogger(ex.toString());
+		} catch (IOException ex) {
+			Logger.getLogger(Communication.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+
+		if (ss.isConnected()) {
+			String response = null;
 			try {
-				dis.close();
-			} catch (IOException e1) {
-				LCD.drawString("Failed to close DataInputStream", 0, 0);
+				response = ss.sendAndReceive("1");
+				System.out.println("Response  " + response);
+				response = ss.sendAndReceive("2");
+				System.out.println("Response  " + response);
+				response = ss.sendAndReceive("3");
+				System.out.println("Response  " + response);
+			} catch (IOException ex) {
+				Logger.getLogger(Communication.class.getName()).log(
+						Level.SEVERE, null, ex);
 			}
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e1) {
-				LCD.drawString("Sleep", 0, 0);
-			} 
-			// wait for data to drain
-			LCD.clear();
-			LCD.drawString("Closing",0,0);
-			LCD.refresh();
-			try {
-				connection.close();
-			} catch (IOException e) {
-				LCD.drawString("Failed to close connection", 0, 0);
-				e.printStackTrace();
-			}
-			LCD.clear();*/
 		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
+
 public class Main {
 	
 	public static void main(String [] args){
+		int timeout = 10000;
+		Communication ms;
+		try {
+			ms = new Communication(6666, timeout);
+			ms.theMain();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		Graph graph = new Graph();
 		Gyro gyro = new Gyro();
-		Communication m = new Communication();
+		//Communication m = new Communication();
 		//gyro.start();
 		Regul regul = new Regul(gyro,2); //Next highest priority.
 									//Reason is to get more accurate samples.
