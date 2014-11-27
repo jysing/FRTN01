3029d4e445bb9c153732859c1e2cd16b72a6d8a3
suburbanOrfeus <nicklas.lth@gmail.com>
2014-11-27 14:59:08 +0100

    added simpson

---
+++ src/SimpsonIteration.java
@@ -0,0 +1,30 @@
+
+public class SimpsonIteration {
+
+		
+		   public static double f(double x) {
+		      return Math.exp(- x * x / 2) / Math.sqrt(2 * Math.PI);
+		   }
+		   
+		   public static double integrate(double a, double b, double n) {
+		      double h = (b - a) / (n - 1);     // step size
+		 
+		      // 1/3 terms
+		      double sum = 1.0 / 3.0 * (f(a) + f(b));
+
+		      // 4/3 terms
+		      for (int i = 1; i < n - 1; i += 2) {
+		         double x = a + h * i;
+		         sum += 4.0 / 3.0 * f(x);
+		      }
+
+		      // 2/3 terms
+		      for (int i = 2; i < n - 1; i += 2) {
+		         double x = a + h * i;
+		         sum += 2.0 / 3.0 * f(x);
+		      }
+
+		      return sum * h;
+		   }
+}
+//http://introcs.cs.princeton.edu/java/93integration/SimpsonsRule.java.html
