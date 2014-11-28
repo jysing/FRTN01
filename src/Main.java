8c08e7daeab16e1a68e001391c36d81d79bdd733
suburbanOrfeus <nicklas.lth@gmail.com>
2014-11-28 10:38:14 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,20 @@ import java.io.IOException;
 import lejos.hardware.lcd.LCD;
 
 public class Main {
-
+	private static final int port = 6666, timeout = 30000;
+	private static final int regulPrio = 2, graphPrio = 1;
 	public static void main(String [] args){
-		int port = 6666;
-		int timeout = 30000;
+		Gyro gyro = new Gyro();
+		Regul regul = new Regul(gyro, regulPrio);
 		Communication comm = null;
 		try {
-			comm = new Communication(port, timeout);
+			comm = new Communication(regul, port, timeout, graphPrio);
 			comm.connect();
 			comm.start();
 		} catch (IOException e) {
 			LCD.drawString("massive connection error", 0, 0);
 		}
-<<<<<<< HEAD
-		
-		Gyro gyro = new Gyro();
-		Regul regul = new Regul(gyro,comm, 2);
-		regul.start();
-		
-=======
 		regul.start();		
->>>>>>> eeb3968c94580bc0557f4eef6dad851f26546e91
 	}
 
 }
