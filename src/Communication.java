f596192364bd099a768db5c28c4b1099b82052ad
Raddle <ael10rpe@student.lu.se>
2014-11-28 10:31:50 +0100

    Gyro Offset enligt hitechnic

--- src/Communication.java
+++ src/Communication.java
@@ -8,7 +8,7 @@ import lejos.hardware.lcd.LCD;
 
 public class Communication extends Thread {
 
-	private static final long period = 10;
+	private static final long period = 20;
 	private final ServerSocket serverSocket;
 	private Socket server;
 	private DataOutputStream out;
@@ -24,10 +24,24 @@ public class Communication extends Thread {
 	}
 
 	public void run() {
+		int i = 0;
 		while (true) {
-			if(this.isConnected()) {
-				String message = String.valueOf(regul.getU());
-				send(message);				
+			if (this.isConnected()) {
+				String message = "Fel";
+				if (i == 0) {
+					message = "U" + String.valueOf(regul.getU());
+				} else if (i == 1) {
+					message = "E" + String.valueOf(regul.getE());
+				} else if (i == 2) {
+					message = "A" + String.valueOf(regul.getA());
+				} else if (i == 3) {
+					message = "V" + String.valueOf(regul.getV());
+				} else {
+					LCD.drawString("Something is very wrong with i", 0, 4);
+				}
+				i++;
+				i = i % 4;
+				send(message);
 			} else {
 				LCD.drawString("It is not connected", 0, 3);
 			}
--- src/Regul.java
+++ src/Regul.java
@@ -10,7 +10,7 @@ public class Regul extends Thread {
 	EncoderMotor motorA;
 	EncoderMotor motorB;
 	
-	private double u; // Control signal from PID
+	private double u, e; // Control signal to/from PID
 	private double angVel, ang; // angluarVelocity and current angle
 	private static final double weightAng = 1, weightAngVel = 2;
 
@@ -60,11 +60,13 @@ public class Regul extends Thread {
 	}
 
 	public void run() {
+		setMotor(0);
     	calculateOffset();
     	while (true) {
     		angVel = (double)gyro.getAngleVelocity();
     		ang = (double)gyro.getAngle();
-    		u = pid.calculateOutput(weightAngVel*angVel+weightAng*ang, 0);
+    		e = weightAngVel*angVel+weightAng*ang;
+    		u = pid.calculateOutput(e, 0);
     		pid.updateState(u);
     		setMotor(u);
     	}
@@ -89,6 +91,18 @@ public class Regul extends Thread {
     public double getU() {
     	return u;
     }
+
+	public double getE() {
+		return e;
+	}
+	
+	public double getA() {
+		return ang;
+	}
+	
+	public double getV() {
+		return angVel;
+	}
 }
 
 
