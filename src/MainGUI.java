0210ee996f6439c1e295324547de507e9ebe033a
Fatmajk <michael.kapusta@live.se>
2014-12-02 16:27:45 +0100

    Button added.

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,15 +1,17 @@
+import java.io.IOException;
+
 public class MainGUI {
 	private static final String serverName = "10.0.1.1";
 	private static final int port = 6666;
 
 	public static void main(String[] args) {
 		SocketClient sc = new SocketClient(serverName, port);
-		/*try {
+		try {
 	    	sc.connect();
 	    } catch (IOException e) {
 	        System.out.println("Failed to connect");
 	    }
-	    */
+		
 	    try {
 		Graph graph = new Graph(sc);	
 		
