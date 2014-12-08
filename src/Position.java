52b476b18fc2fd575f378d24f9c1f4b76747f433
suburbanOrfeus <nicklas.lth@gmail.com>
2014-12-08 15:12:00 +0100

    Rich suger

--- src/Position.java
+++ src/Position.java
@@ -4,7 +4,7 @@ import lejos.robotics.EncoderMotor;
 public class Position {
 	private long time, difference;
 	private EncoderMotor motorA;
-	private double oldValue, value, tempValue, filterValue, oldFilterValue;
+	private double oldValue, value, tempValue, filterValue, oldFilterValue, filterConstant;
 	private double meterPerDegree;
 	private boolean reset;
 
@@ -16,8 +16,9 @@ public class Position {
 		tempValue = 0;
 		meterPerDegree = 0.000697778;
 		reset = false;
-		filterValue = 1;
+		filterValue = 0;
 		oldFilterValue = 0;
+		filterConstant = 0.1;
 	}
 
 	public double getPosVelocity() {
@@ -27,11 +28,12 @@ public class Position {
 			tempValue = getPosition();
 			value = ((tempValue - oldValue) / difference);
 			oldValue = tempValue;
-			filterValue = (-(difference-2)/(difference+2))*oldFilterValue+
-					((difference/(difference+2))*value)+((difference/(difference+2))*oldValue);
+			filterValue = value - filterConstant*oldFilterValue;
+					/*(-(difference-2)/(difference+2))*oldFilterValue+
+					((difference/(difference+2))*value)+((difference/(difference+2))*oldValue);*/
 			oldFilterValue = filterValue;
 		}
-		return 10;
+		return filterValue;
 	}
 
 	public double getPosition() {
