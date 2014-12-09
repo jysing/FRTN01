6d832842b67ee078067e5adf84bc9c3df84b0816
suburbanOrfeus <nicklas.lth@gmail.com>
2014-12-09 11:31:27 +0100

    reset fix

--- src/Position.java
+++ src/Position.java
@@ -3,10 +3,10 @@ import lejos.robotics.EncoderMotor;
 public class Position {
 	private long time, difference;
 	private EncoderMotor motorA;
-	private double oldValue, value, tempValue, filterValue, oldFilterValue,
+	private double oldValue, value, tempValue, filterValue, oldFilterValue, preReset,
 			filterConstant;
 	private double meterPerDegree;
-	private boolean reset;
+	//private boolean reset;
 
 	public Position(EncoderMotor motorA) {
 		this.motorA = motorA;
@@ -15,10 +15,11 @@ public class Position {
 		value = 0;
 		tempValue = 0;
 		meterPerDegree = 0.000697778;
-		reset = false;
+		//reset = false;
 		filterValue = 0;
 		oldFilterValue = 0;
 		filterConstant = 0.1;
+		preReset = 0;
 	}
 
 	public synchronized double getPosVelocity() {
@@ -40,18 +41,19 @@ public class Position {
 	}
 
 	public synchronized double getPosition() {
-		if (reset) {
+		/*if (reset) {
 			motorA.resetTachoCount();
 			reset = false;
 			return 0;
-		} else {
-			return motorA.getTachoCount() * meterPerDegree;
-		}
+		} else {*/
+			return (motorA.getTachoCount()-preReset )* meterPerDegree;
+		//}
 	}
 
 	public synchronized void reset() {
-		motorA.resetTachoCount();
-		reset = true;
+		//motorA.resetTachoCount();
+		preReset += motorA.getTachoCount();
+		//reset = true;
 		tempValue = 0;
 		oldValue = 0;
 		value = 0;
